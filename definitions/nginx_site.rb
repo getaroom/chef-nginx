3b6106af23e93f77d348528cff985176fc0d3135
Chris Griego <cgriego@gmail.com>
2012-08-22 15:51:58 -0500

    Fix default site always reloading nginx

--- definitions/nginx_site.rb
+++ definitions/nginx_site.rb
@@ -19,17 +19,19 @@
 #
 
 define :nginx_site, :enable => true do
+  enabled_name = params[:name] == "default" ? "000-default" : params[:name]
+
   if params[:enable]
     execute "nxensite #{params[:name]}" do
       command "/usr/sbin/nxensite #{params[:name]}"
       notifies :reload, resources(:service => "nginx")
-      not_if do ::File.symlink?("#{node['nginx']['dir']}/sites-enabled/#{params[:name]}") end
+      not_if do ::File.symlink?("#{node['nginx']['dir']}/sites-enabled/#{enabled_name}") end
     end
   else
     execute "nxdissite #{params[:name]}" do
       command "/usr/sbin/nxdissite #{params[:name]}"
       notifies :reload, resources(:service => "nginx")
-      only_if do ::File.symlink?("#{node['nginx']['dir']}/sites-enabled/#{params[:name]}") end
+      only_if do ::File.symlink?("#{node['nginx']['dir']}/sites-enabled/#{enabled_name}") end
     end
   end
 end
